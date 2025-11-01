adf456a6d5e0151d8720ed8981311b089f6c0bd0
xl-immersiv <80410207+laborio@users.noreply.github.com>
2025-11-01 14:18:01 +0100

    [Add] new design scene for character control with climbing bugged
    design scene :
    -unity terrain based ground
    - player controller click to move
    - animations: run/walk/climb

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,106 +1,99 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private enum MovementMode
-    {
-        ClickToMove,
-        Keyboard
-    }
-
-    [Header("General Settings")]
-    [SerializeField] private float moveSpeed = 6f;
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 6f;
     [SerializeField] private float runMultiplier = 1.5f;
-    [SerializeField] private float acceleration = 20f;
-    [SerializeField] private float rotationSpeed = 720f;
-    [SerializeField] private LayerMask groundMask = ~0;
-    [SerializeField] private KeyCode toggleInputKey = KeyCode.F1;
-    [SerializeField] private MovementMode startingMode = MovementMode.ClickToMove;
-    [SerializeField] private KeyCode moveLeftKey = KeyCode.Q;
-    [SerializeField] private KeyCode moveRightKey = KeyCode.D;
-    [SerializeField] private KeyCode moveForwardKey = KeyCode.Z;
-    [SerializeField] private KeyCode moveBackwardKey = KeyCode.S;
+
+    [Header("Acceleration")]
+    [SerializeField] private float walkAcceleration = 20f;
+    [SerializeField] private float runAcceleration = 30f;
+
+    [Header("Rotation")]
+    [SerializeField] private float walkRotationSpeed = 720f;
+    [SerializeField] private float runRotationSpeed = 900f;
+    [SerializeField] private float climbRotationSpeed = 540f;
+    [SerializeField] private float climbSpeed = 3f;
+    [SerializeField] private float climbAnimationBaseSpeed = 1f;
+    [SerializeField] private float finishClimbSpeed = 2f;
+    [SerializeField] private float finishClimbAnimationBaseSpeed = 1f;
+
+    [Header("Input")]
     [SerializeField] private KeyCode runKey = KeyCode.Space;
+    [SerializeField] private KeyCode stopKey = KeyCode.S;
+
+    [Header("Animation")]
+    [SerializeField] private Animator animator;
+    [SerializeField] private float walkAnimationBaseSpeed = 1f;
+    [SerializeField] private float runAnimationBaseSpeed = 1f;
 
     [Header("Click To Move")]
     [SerializeField] private float stopDistance = 0.2f;
     [SerializeField] private float maximumRayDistance = 250f;
+    [SerializeField] private LayerMask groundMask = ~0;
     [SerializeField] private ClickMoveMarkerPool moveMarkerPool;
     [SerializeField] private float markerHeight = 0.1f;
 
-    private MovementMode _currentMode;
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem runParticleSystem;
+
+    private static readonly int IsRunningHash = Animator.StringToHash("isRunning");
+    private static readonly int IsWalkingHash = Animator.StringToHash("isWalking");
+    private static readonly int IsIdleHash = Animator.StringToHash("isIdle");
+    private static readonly int IsClimbingHash = Animator.StringToHash("isClimbing");
+    private static readonly int FinishClimbingHash = Animator.StringToHash("FinishClimbing");
+
     private Vector3 _moveTarget;
     private bool _hasMoveTarget;
-    private Vector3 _keyboardInput;
     private Vector3 _currentVelocity;
+    private bool _runInputActive;
+    private bool _isRunningThisFrame;
+    private bool _isClimbing;
+    private Vector3 _climbFacingDirection;
+    private bool _hasClimbFacingDirection;
+    private HashSet<Collider> _activeClimbContacts;
+    private int _climbableLayer;
+    private bool _finishClimbing;
 
     private void Awake()
     {
-        _currentMode = startingMode;
+        _activeClimbContacts = new HashSet<Collider>();
+        _climbableLayer = LayerMask.NameToLayer("Climbable");
     }
 
     private void Update()
     {
-        HandleModeToggle();
+        HandleStopCommand();
+        HandleClickToMoveInput();
 
-        if (_currentMode == MovementMode.ClickToMove)
+        if (_isClimbing)
         {
-            HandleClickToMoveInput();
+            _hasMoveTarget = false;
         }
-        else
-        {
-            HandleKeyboardInput();
-        }
+
+        _runInputActive = Input.GetKey(runKey);
 
         MovePlayer();
-        HandleRotation();
-    }
 
-    private void MovePlayer()
-    {
-        Vector3 desiredVelocity = Vector3.zero;
-        float currentSpeed = moveSpeed;
+        UpdateAnimationState();
 
-        if (Input.GetKey(runKey))
+        if (_isClimbing || _finishClimbing)
         {
-            currentSpeed *= runMultiplier;
-        }
-
-        if (_currentMode == MovementMode.ClickToMove)
-        {
-            if (_hasMoveTarget)
-            {
-                Vector3 toTarget = _moveTarget - transform.position;
-                toTarget.y = 0f;
-
-                if (toTarget.magnitude <= stopDistance)
-                {
-                    _hasMoveTarget = false;
-                }
-                else
-                {
-                    desiredVelocity = toTarget.normalized * currentSpeed;
-                }
-            }
+            HandleClimbRotation();
         }
         else
         {
-            desiredVelocity = _keyboardInput.normalized * currentSpeed;
+            HandleRotation();
         }
-
-        ApplyMovement(desiredVelocity);
     }
 
-    private void HandleModeToggle()
+    private void HandleStopCommand()
     {
-        if (Input.GetKeyDown(toggleInputKey))
+        if (Input.GetKeyDown(stopKey))
         {
-            _currentMode = _currentMode == MovementMode.ClickToMove
-                ? MovementMode.Keyboard
-                : MovementMode.ClickToMove;
-
             _hasMoveTarget = false;
-            _keyboardInput = Vector3.zero;
             _currentVelocity = Vector3.zero;
         }
     }
@@ -128,50 +121,170 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void HandleKeyboardInput()
+    private void MovePlayer()
     {
-        float horizontal = 0f;
-        float vertical = 0f;
-
-        if (Input.GetKey(moveLeftKey))
+        if (_isClimbing)
         {
-            horizontal -= 1f;
+            _currentVelocity = Vector3.up * climbSpeed;
+            transform.position += _currentVelocity * Time.deltaTime;
+            _isRunningThisFrame = false;
+            return;
         }
-        if (Input.GetKey(moveRightKey))
+        if (_finishClimbing)
         {
-            horizontal += 1f;
-        }
-        if (Input.GetKey(moveForwardKey))
-        {
-            vertical += 1f;
-        }
-        if (Input.GetKey(moveBackwardKey))
-        {
-            vertical -= 1f;
+            _currentVelocity = Vector3.up * finishClimbSpeed;
+            transform.position += _currentVelocity * Time.deltaTime;
+            _isRunningThisFrame = false;
+            return;
         }
 
-        Vector3 planarInput = new Vector3(horizontal, 0f, vertical);
-        _keyboardInput = planarInput.magnitude > 1f ? planarInput.normalized : planarInput;
-    }
+        Vector3 desiredVelocity = Vector3.zero;
 
-    private void ApplyMovement(Vector3 desiredVelocity)
-    {
-        desiredVelocity.y = 0f;
-        _currentVelocity = Vector3.MoveTowards(_currentVelocity, desiredVelocity, acceleration * Time.deltaTime);
-
-        Vector3 displacement = _currentVelocity * Time.deltaTime;
-        transform.position += displacement;
+        bool wantsToRun = _runInputActive && _hasMoveTarget;
+        float targetSpeed = walkSpeed * (wantsToRun ? runMultiplier : 1f);
+        float currentAcceleration = wantsToRun ? runAcceleration : walkAcceleration;
 
         if (_hasMoveTarget)
         {
             Vector3 toTarget = _moveTarget - transform.position;
             toTarget.y = 0f;
+
             if (toTarget.magnitude <= stopDistance)
+            {
+                _hasMoveTarget = false;
+            }
+            else
+            {
+                desiredVelocity = toTarget.normalized * targetSpeed;
+            }
+        }
+
+        _currentVelocity = Vector3.MoveTowards(_currentVelocity, desiredVelocity, currentAcceleration * Time.deltaTime);
+        transform.position += _currentVelocity * Time.deltaTime;
+
+        if (_hasMoveTarget)
+        {
+            Vector3 remaining = _moveTarget - transform.position;
+            remaining.y = 0f;
+            if (remaining.magnitude <= stopDistance)
             {
                 _hasMoveTarget = false;
                 _currentVelocity = Vector3.zero;
             }
         }
+
+        _isRunningThisFrame = wantsToRun && _currentVelocity.sqrMagnitude > 0.0001f;
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (_isClimbing)
+        {
+            UpdateRunParticles(false);
+
+            if (!animator)
+            {
+                return;
+            }
+
+            animator.SetBool(IsClimbingHash, true);
+            animator.SetBool(FinishClimbingHash, false);
+            animator.SetBool(IsRunningHash, false);
+            animator.SetBool(IsWalkingHash, false);
+            animator.SetBool(IsIdleHash, false);
+            animator.speed = climbAnimationBaseSpeed;
+            return;
+        }
+        if (_finishClimbing)
+        {
+            UpdateRunParticles(false);
+
+            if (!animator)
+            {
+                return;
+            }
+
+            animator.SetBool(IsClimbingHash, false);
+            animator.SetBool(FinishClimbingHash, true);
+            animator.SetBool(IsRunningHash, false);
+            animator.SetBool(IsWalkingHash, false);
+            animator.SetBool(IsIdleHash, false);
+            animator.speed = finishClimbAnimationBaseSpeed;
+            return;
+        }
+
+        Vector3 planarVelocity = new Vector3(_currentVelocity.x, 0f, _currentVelocity.z);
+        float speed = planarVelocity.magnitude;
+        bool isMoving = speed > 0.01f;
+
+        bool isRunning = _isRunningThisFrame && isMoving;
+        bool isWalking = !isRunning && isMoving;
+        bool isIdle = !isRunning && !isWalking;
+
+        UpdateRunParticles(isRunning);
+
+        if (!animator)
+        {
+            return;
+        }
+
+        animator.SetBool(IsClimbingHash, false);
+        animator.SetBool(FinishClimbingHash, false);
+        animator.SetBool(IsRunningHash, isRunning);
+        animator.SetBool(IsWalkingHash, isWalking);
+        animator.SetBool(IsIdleHash, isIdle);
+
+        if (isRunning)
+        {
+            float runSpeed = Mathf.Max(0.001f, walkSpeed * runMultiplier);
+            float normalized = Mathf.Clamp(speed / runSpeed, 0f, 2f);
+            animator.speed = runAnimationBaseSpeed * normalized;
+        }
+        else if (isWalking)
+        {
+            float normalized = Mathf.Clamp(speed / Mathf.Max(0.001f, walkSpeed), 0f, 2f);
+            animator.speed = walkAnimationBaseSpeed * normalized;
+        }
+        else
+        {
+            animator.speed = 1f;
+        }
+    }
+
+    private void UpdateRunParticles(bool shouldBeActive)
+    {
+        if (!runParticleSystem)
+        {
+            return;
+        }
+
+        if (shouldBeActive)
+        {
+            ParticleSystem.EmissionModule emission = runParticleSystem.emission;
+            if (!emission.enabled)
+            {
+                emission.enabled = true;
+            }
+        }
+        else
+        {
+            ParticleSystem.EmissionModule emission = runParticleSystem.emission;
+            if (emission.enabled)
+            {
+                emission.enabled = false;
+            }
+        }
+    }
+
+    private void HandleClimbRotation()
+    {
+        if (!_hasClimbFacingDirection)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(_climbFacingDirection, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, climbRotationSpeed * Time.deltaTime);
     }
 
     private void HandleRotation()
@@ -182,9 +295,137 @@ public class PlayerController : MonoBehaviour
             return;
         }
 
+        float currentRotationSpeed = _isRunningThisFrame ? runRotationSpeed : walkRotationSpeed;
         Quaternion targetRotation = Quaternion.LookRotation(planarVelocity.normalized, Vector3.up);
-        Quaternion newRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        Quaternion newRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, currentRotationSpeed * Time.deltaTime);
         transform.rotation = newRotation;
+    }
+
+    private void TryBeginClimb(Collision collision)
+    {
+        if (_finishClimbing)
+        {
+            return;
+        }
+
+        Collider collider = collision.collider;
+        if (!IsClimbableCollider(collider))
+        {
+            return;
+        }
+
+        _activeClimbContacts.Add(collider);
+        _isClimbing = true;
+        _currentVelocity = Vector3.zero;
+        _isRunningThisFrame = false;
+        UpdateClimbFacingDirection(collision);
+    }
+
+    private void UpdateClimbFacingDirection(Collision collision)
+    {
+        _hasClimbFacingDirection = false;
+
+        if (collision.contactCount > 0)
+        {
+            Vector3 averagedNormal = Vector3.zero;
+            ContactPoint[] contacts = collision.contacts;
+            for (int i = 0; i < contacts.Length; i++)
+            {
+                averagedNormal += contacts[i].normal;
+            }
+
+            if (averagedNormal.sqrMagnitude > 0.0001f)
+            {
+                averagedNormal.Normalize();
+                Vector3 facing = -averagedNormal;
+                facing.y = 0f;
+                if (facing.sqrMagnitude > 0.0001f)
+                {
+                    _climbFacingDirection = facing.normalized;
+                    _hasClimbFacingDirection = true;
+                    return;
+                }
+            }
+        }
+
+        Vector3 directionToCollider = collision.collider.transform.position - transform.position;
+        directionToCollider.y = 0f;
+        if (directionToCollider.sqrMagnitude > 0.0001f)
+        {
+            _climbFacingDirection = directionToCollider.normalized;
+            _hasClimbFacingDirection = true;
+        }
+    }
+
+    private bool IsClimbableCollider(Collider collider)
+    {
+        if (!collider || !collider.CompareTag("Wall"))
+        {
+            return false;
+        }
+
+        if (_climbableLayer != -1 && collider.gameObject.layer != _climbableLayer)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        TryBeginClimb(collision);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        TryBeginClimb(collision);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (!IsClimbableCollider(collision.collider))
+        {
+            return;
+        }
+
+        _activeClimbContacts.Remove(collision.collider);
+        if (_activeClimbContacts.Count == 0 && !_finishClimbing)
+        {
+            _isClimbing = false;
+            _hasClimbFacingDirection = false;
+            _climbFacingDirection = Vector3.zero;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("StopClimb"))
+        {
+            return;
+        }
+
+        FinishClimb();
+    }
+
+    private void FinishClimb()
+    {
+        if (_finishClimbing || !_isClimbing)
+        {
+            return;
+        }
+
+        _finishClimbing = true;
+        _isClimbing = false;
+        _currentVelocity = Vector3.zero;
+        _hasMoveTarget = false;
+        _activeClimbContacts.Clear();
+
+        if (animator)
+        {
+            animator.SetBool(IsClimbingHash, false);
+            animator.SetBool(FinishClimbingHash, true);
+        }
     }
 
     private void SpawnMoveMarker(Vector3 position)
@@ -197,5 +438,54 @@ public class PlayerController : MonoBehaviour
         Vector3 markerPosition = position;
         markerPosition.y = markerHeight;
         moveMarkerPool.SpawnMarker(markerPosition);
+    }
+
+    private void OnDisable()
+    {
+        UpdateRunParticles(false);
+        _isClimbing = false;
+        _hasClimbFacingDirection = false;
+        _climbFacingDirection = Vector3.zero;
+        _finishClimbing = false;
+        _activeClimbContacts?.Clear();
+
+        if (animator)
+        {
+            animator.SetBool(IsClimbingHash, false);
+            animator.SetBool(FinishClimbingHash, false);
+        }
+    }
+
+    private void OnValidate()
+    {
+        walkSpeed = Mathf.Max(0f, walkSpeed);
+        runMultiplier = Mathf.Max(1f, runMultiplier);
+        walkAcceleration = Mathf.Max(0f, walkAcceleration);
+        runAcceleration = Mathf.Max(0f, runAcceleration);
+        walkRotationSpeed = Mathf.Max(0f, walkRotationSpeed);
+        runRotationSpeed = Mathf.Max(0f, runRotationSpeed);
+        climbRotationSpeed = Mathf.Max(0f, climbRotationSpeed);
+        climbSpeed = Mathf.Max(0f, climbSpeed);
+        finishClimbSpeed = Mathf.Max(0f, finishClimbSpeed);
+        stopDistance = Mathf.Max(0f, stopDistance);
+        maximumRayDistance = Mathf.Max(0f, maximumRayDistance);
+        markerHeight = Mathf.Max(0f, markerHeight);
+        walkAnimationBaseSpeed = Mathf.Max(0.01f, walkAnimationBaseSpeed);
+        runAnimationBaseSpeed = Mathf.Max(0.01f, runAnimationBaseSpeed);
+        climbAnimationBaseSpeed = Mathf.Max(0.01f, climbAnimationBaseSpeed);
+        finishClimbAnimationBaseSpeed = Mathf.Max(0.01f, finishClimbAnimationBaseSpeed);
+    }
+
+    public void ResetFinishClimbFlag()
+    {
+        _finishClimbing = false;
+        _currentVelocity = Vector3.zero;
+        _hasClimbFacingDirection = false;
+        _climbFacingDirection = Vector3.zero;
+
+        if (animator)
+        {
+            animator.SetBool(FinishClimbingHash, false);
+        }
     }
 }
