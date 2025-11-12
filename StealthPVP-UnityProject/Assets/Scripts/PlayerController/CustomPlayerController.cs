2147d7c8dafab14e5fdc1bc93f10da0c0f769fdd
Thanaloc <raphael.seguin.97@gmail.com>
2025-11-12 01:18:31 +0100

    [Add] Fonctionnal basic multiplayer

--- StealthPVP-UnityProject/Assets/Scripts/PlayerController/CustomPlayerController.cs
+++ StealthPVP-UnityProject/Assets/Scripts/PlayerController/CustomPlayerController.cs
@@ -1,33 +1,74 @@
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-public class CustomPlayerController : MonoBehaviour
+public class CustomPlayerController : NetworkBehaviour
 {
+    #region SRs
+    [Header("Player variables")]
     [SerializeField] private PlayerConfigSO _PlayerConfig;
     [SerializeField] private PlayerInputHandler _PlayerInputs;
     [SerializeField] private CharacterController _CharacterController;
-    [SerializeField] private Transform _PlayerRoot;
+    [SerializeField] private GameObject _IsoPlayerCam;
 
+    [Header("Transforms")]
+    [SerializeField] private Transform _PlayerRoot;
     [SerializeField] private Transform _PlayerCam;
+    #endregion
+
+    #region Private var
 
     private Vector3 _velocity,
         _cameraForwardAxis,
         _cameraRightAxis,
         _moveDirection;
 
-    private Vector2 _input;
-
+    private Quaternion _targetRotation = new();
     private float _currentSpeed = 0f;
 
-    private Quaternion _targetRotation = new();
+    private Vector2 _input;
+
+    private NetworkVariable<Vector3> _networkPosition = new NetworkVariable<Vector3>();
+    private NetworkVariable<Quaternion> _networkRotation = new NetworkVariable<Quaternion>();
+
+    private float _lastJumpTime = -999f;
+
+    #endregion
+
+    #region Mono
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+
+        if (!IsOwner)
+        {
+            _PlayerCam.gameObject.SetActive(false);
+            _IsoPlayerCam.gameObject.SetActive(false);
+            //enabled = false; à remettre si on fait client side
+            //return;
+        }
+
+        else
+        {
+            enabled = true;
+        }
+    }
 
     private void Update()
     {
+        if (!IsOwner)
+            return;
+
         HandleMovement();
+        HandleJump();
         HandleGravity();
     }
 
+    #endregion
+
+    #region Physics
     private void HandleMovement()
     {
         _input = _PlayerInputs.MoveInput;
@@ -48,16 +89,26 @@ public class CustomPlayerController : MonoBehaviour
 
         _currentSpeed = _PlayerInputs.IsWalking ? _PlayerConfig.WalkSpeed : _PlayerConfig.DefaultSpeed;
 
-        _CharacterController.Move(_moveDirection * _currentSpeed * Time.deltaTime);
+        MoveAndRotateServerRpc(_moveDirection, _currentSpeed); //à enelevr si on veut faire client side
 
+        //_CharacterController.Move(_moveDirection * _PlayerConfig.RotationSpeed * Time.deltaTime); // à remettre si on veut faire client side
+        //HandleRotation(_moveDirection, _PlayerConfig.RotationSpeed);
+
+        //_networkPosition.Value = _moveDirection * _currentSpeed * Time.deltaTime;
+        //_networkRotation.Value = _targetRotation;
+    }
+
+
+    private void HandleRotation(Vector3 p_moveDir, float p_rotationSpeed)
+    {
         //rotation
-        if (_moveDirection.sqrMagnitude > .01f)
+        if (p_moveDir.sqrMagnitude > .01f)
         {
-            _targetRotation = Quaternion.LookRotation(_moveDirection);
+            _targetRotation = Quaternion.LookRotation(p_moveDir);
             _PlayerRoot.rotation = Quaternion.Slerp(
                 _PlayerRoot.rotation,
                 _targetRotation,
-                _PlayerConfig.RotationSpeed * Time.deltaTime);
+                p_rotationSpeed * Time.deltaTime);
         }
     }
 
@@ -69,6 +120,38 @@ public class CustomPlayerController : MonoBehaviour
         }
 
         _velocity.y += _PlayerConfig.Gravity * Time.deltaTime;
+        //_CharacterController.Move(_velocity * Time.deltaTime); //à remettre si on veut client side
+
+        ApplyGravityServerRpc(_velocity); //à enlever si on veut client side
+    }
+
+    private void HandleJump()
+    {
+        if (_PlayerInputs.IsJumping && _CharacterController.isGrounded && Time.time >= _lastJumpTime + _PlayerConfig.JumpCooldown)
+        {
+            _velocity.y = Mathf.Sqrt(_PlayerConfig.JumpHeight * -2f * _PlayerConfig.Gravity);
+            _lastJumpTime = Time.time;
+            _PlayerInputs.ResetJump();
+        }
+    }
+
+
+    #endregion 
+
+    #region RPC
+
+    [ServerRpc]
+    private void MoveAndRotateServerRpc(Vector3 p_moveDir, float p_speed)
+    {
+        _CharacterController.Move(p_moveDir * p_speed * Time.deltaTime);
+        HandleRotation(p_moveDir, _PlayerConfig.RotationSpeed);
+    }
+
+    [ServerRpc]
+    private void ApplyGravityServerRpc(Vector3 p_velocity)
+    {
         _CharacterController.Move(_velocity * Time.deltaTime);
     }
+
+    #endregion
 }
--- StealthPVP-UnityProject/Assets/Scripts/PlayerController/ScriptableObjects/PlayerConfigSO.cs
+++ StealthPVP-UnityProject/Assets/Scripts/PlayerController/ScriptableObjects/PlayerConfigSO.cs
@@ -6,19 +6,21 @@ public class PlayerConfigSO : ScriptableObject
     [Header("Speed Values")]
     public float DefaultSpeed = 5f;
     public float WalkSpeed = 2.5f;
-    public float Acceleration = 2f;
-    public float Deceleration = 2f;
+    //public float Acceleration = 2f;
+    //public float Deceleration = 2f;
     public float RotationSpeed = 10f;
-
-    public bool RotateTowardMovement = true;
 
     [Header("Air values")]
     public float Gravity = -9.81f;
     public float GroundedGravity = -2f;
-    public float MaxFallSpeed = -8f;
+    //public float MaxFallSpeed = -8f;
 
     [Header("Ground Checking")]
     public float GroundCheckDistance = 0.2f;
     public LayerMask GroundMask = -1;
+
+    [Header("Jump Variables")]
+    public float JumpHeight = 2f;
+    public float JumpCooldown = 0.5f;
 
 }
