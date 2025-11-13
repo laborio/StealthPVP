b5d062fc92ff02b4974e5c419334f1ed510601a3
Rapha <raph4l3@gmail.com>
2025-11-13 16:06:54 +0100

    [Add] new prefabs for easy proto

--- StealthPVP-UnityProject/Assets/Scripts/testmpmanager.cs
+++ StealthPVP-UnityProject/Assets/Scripts/testmpmanager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class testmpmanager : MonoBehaviour
 {
     public Camera cam;
-    public Canvas canvas;
+    public GameObject canvas;
     public void OnBtnClick()
     {
         cam.gameObject.SetActive(false);
